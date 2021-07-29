8d3f1544987cf65afdfa5a14001e5fd6514b66ea
Leroll <senjaer1919@gmail.com>
2021-07-29 23:35:50 +0800

    1. 改进time cost 装饰器 2. 加入logging, 使用loguru包.

--- utils.py
+++ utils.py
@@ -1,15 +1,33 @@
 from time import time
+from loguru import logger
 
 
-def call_time_with_name(name):
-    """
-    计时装饰器
-    """
-    def call_time(func):
-        def wraps(*args, **kargs):
-            t0 = time()
-            result = func(*args, **kargs)
-            print(f'{name}用时: {time() - t0:0.3f}')
-            return result
-        return wraps
-    return call_time
+def time_cost(func):
+    def wraps(*args, **kargs):
+        t0 = time()
+        result = func(*args, **kargs)
+        print(f'[{func.__name__}] cost time: {time() - t0:0.3f}s')
+        return result
+    return wraps
+
+
+class LogGetter:
+    def __init__(self):
+        self.logger = logger
+        self.logger.add('log.log',
+                        enqueue=True,
+                        colorize=True, format="<green>{time}</green> <level>{message}</level>",
+                        level="INFO"
+                        )
+
+    def get_logger(self):
+        return self.logger.info
+
+
+log = LogGetter().get_logger()
+
+if __name__ == '__main__':
+    log('log test')
+
+
+
