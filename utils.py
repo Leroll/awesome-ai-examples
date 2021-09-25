d70d6c6d8c1035944f7ce014d94cd12900e2db06
Leroll <senjaer1919@gmail.com>
2021-09-25 22:23:10 +0800

    1. 新增字典反转函数

--- utils.py
+++ utils.py
@@ -9,3 +9,18 @@ def time_cost(func):
         return res
     return Wrapper
 
+
+def reverse_dict(x:dict):
+    """
+    交换字典的key-value
+    得到 value-key 的新字典
+    需保证 value 无重复项
+    """
+    if isinstance(x, dict):
+        k, v = list(zip(*list(x.items())))
+        x_reverse = {}
+        for i in range(len(k)):
+            x_reverse[v[i]] = k[i]  # k-v 反转字典
+        return x_reverse
+    else:
+        raise TypeError('arg needs to be dict')
