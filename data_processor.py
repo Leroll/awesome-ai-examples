50318a3310e64d6702ab5be7423b0b3b9269fb92
Leroll <senjaer1919@gmail.com>
2021-09-25 21:33:35 +0800

    1. 整理data_processor    提供处理从文件到dataloder过程中的各种方法

---
+++ data_processor.py
@@ -0,0 +1,267 @@
+import pandas as pd
+import numpy as np
+import random
+from multiprocessing import Pool
+import sys
+import torch
+import jieba
+
+
+from utils import time_cost
+
+
+class DataProcessor:
+    """
+    从源文件读取数据到Dataloader过程中的各种工具函数.
+    """
+
+    def __init__(self, logger=print):
+        self.logger = logger
+
+    ############################################
+    # 文件读取
+    ############################################
+
+    @time_cost
+    def read_data(self, mode, name, path, sep, encoder='utf-8', has_index=False):
+        """
+        读取数据,返回 list形式的数据
+        处理如下类型数据集
+        [q1 q2 label]
+
+        [idx q1 q2 label]
+
+        mode: 读取数据的方式,
+              readline
+              pandas
+        """
+        self.logger(f'-' * 42)
+        self.logger(f'start to read: [{name}]...')
+
+        if mode == 'readline':
+            data = self._read_data_by_readline(path=path,
+                                               sep=sep,
+                                               encoder=encoder,
+                                               has_index=has_index)
+        elif mode == 'pandas':
+            data = self._read_data_by_pandas(path=path,
+                                             sep=sep,
+                                             encoder=encoder)
+        else:
+            raise Exception('mode的值有误')
+
+        # logs
+        self.logger(f'finish reading: [{name}]')
+        self.logger('nums:', len(data))
+        for i in range(5):
+            self.logger(data[i])
+        return data
+
+    def _read_data_by_readline(self, path, sep, encoder='utf-8', has_index=False):
+        data = []
+        with open(path, encoding=encoder) as f:
+            line = f.readline()
+            while line:
+                try:
+                    # 预处理
+                    line = line.strip()
+                    line = line.replace('\ufeff', '')
+
+                    if has_index:
+                        idx, q1, q2, label = line.split(sep)
+                    else:
+                        q1, q2, label = line.split(sep)
+                    data.append([q1, q2, label])
+
+                    line = f.readline()
+                except Exception as e:
+                    print(f'line: {line}')
+                    print('-' * 42)
+                    print(e)
+                    sys.exit()
+        return data
+
+    def _read_data_by_pandas(self, path, sep, encoder='utf-8'):
+        data = pd.read_csv(path, sep=sep, encoding=encoder)
+        data = data.to_numpy().tolist()
+        return data
+
+    def create_dataloader(self, data, batch_size, is_shuffle, collate_fn):
+        dataloader = torch.utils.data.DataLoader(data,
+                                                 batch_size=batch_size,
+                                                 shuffle=is_shuffle,
+                                                 collate_fn=collate_fn)
+        return dataloader
+
+    ############################################
+    # preprocessing
+    ############################################
+
+    def preprocessing(self, text:str):
+        """
+        单句处理
+        """
+        global vocab_dict
+
+        res = ''
+        for i in text:
+            # 大写字母vocab里面没有
+            i = i.lower()
+
+            # punctuation
+            if i == '…':
+                i = '.'
+            elif i in ["'", '‘', '’', '“', '”']:
+                i = "'"
+            elif i in ['—', '―', '—', '`']:  # 注意'-'互相不一样
+                i = ','
+
+            # char
+            char_correct = {
+                '壋': '增', '笫': '第', '囙': '回',
+                '呮': '呗', '嚒': '么', '睌': '晚',
+                '谝': '骗', '鍀': '得', '昰': '是',
+                '伲': '呢', '肔': '服', '凊': '清',
+                '挷': '绑', '亊': '事', '腨': '用',
+                '戗': '钱', '玏': '功', '筘': '扣',
+                '鈤': '日', '颃': '领', '讠': '之',
+                '扥': '在', '螚': '能', '甪': '用',
+                '茌': '花', '泝': '没', '牫': '我',
+                '孒': '了', '镸': '长', '欹': '款',
+                '刭': '到', '幵': '开', '怩': '呢',
+                '绐': '给', '弍': '式', '淸': '清',
+                '夂': '久', '叧': '另', '徣': '借',
+                '冋': '回', '敉': '粒', '埭': '贷',
+                '仧': '卡', '頟': '额', '捿': '捷',
+                '鳓': '嘞', '䃼': '补', '囯': '国',
+                '吿': '告'
+            }
+            if i in char_correct:
+                i = char_correct[i]
+
+            # nonsense letter
+            if i in [' ', ' ', '　', '　', ' ', ' ',
+                     chr(8198), chr(65039), chr(8237), chr(8236),  # 一串打出来都是空格
+                     '\u200d', '\x08', '', '',
+                     '∨', '乛', '∵', chr(8198), ]:
+                continue
+
+            # UNK
+            if i not in vocab_dict:
+                self.logger(text, '|', ord(i), '|', i)
+                i = '[UNK]'  # 这个UNK后面的 tokenizer可以处理
+
+            res += i
+
+        return res
+
+    def preprocess_similarity_data(self, data):
+        """
+        预处理形如 [[q1_1,q2_1,label_1], [q1_2,q2_2,label_2]] 的数据
+        """
+        q1, q2, label = list(zip(*data))
+        q1 = [self.preprocessing(q) for q in q1]
+        self.logger('q1 处理完毕')
+        q2 = [self.preprocessing(q) for q in q2]
+        self.logger('q2 处理完毕')
+
+        res = list(zip(q1, q2, label))
+        return res
+
+
+    def word_segmentation(self, s: str):
+        """
+        分词
+
+        #TODO 后续加入 hanlp 分词对比
+        """
+        seg_s = jieba.lcut(s)
+        return seg_s
+
+    #################################################
+    # multiprocessing
+    #################################################
+
+    @time_cost
+    def multi(self, work_num, func, data):
+        """
+        目前函数输入只有 data
+        """
+        per_lenght = len(data) // work_num
+
+        p = Pool()
+        p_res = []
+        for i in range(work_num):
+            begin = per_lenght * i
+            end = per_lenght * (i + 1) if i != (work_num - 1) else len(data)
+            p_res.append(p.apply_async(func, args=(data[begin:end],)))
+            self.logger(f'Process:{i} | [{begin}:{end}] ')
+        p.close()
+        p.join()
+
+        res = []
+        for i in range(work_num):
+            res.append(p_res[i].get())
+
+        # 拼装起来,外层循环得到单进程小res，内层循环得到j
+        res = [j for i in res for j in i]
+        return res
+
+    ##################################################
+    # mask data
+    ##################################################
+
+    def get_masked_text(self, text):
+        """
+        生成masked后的输入inputs
+        和对应的输出标签 labels
+        为了利用tokenizer, labels也生成文字序列, 不预测的部分用[PAD]代替
+        """
+        global vocab_dict
+
+        inputs = []
+        labels = []
+
+        # 获取 masked tokens
+        masked_num = max(1, round(len(text) * 0.15))
+        masked_tokens = random.sample(text, masked_num)
+
+        for i in text:
+            if i not in vocab_dict:
+                print(f'UNK | {text} | {ord(i)} | {i}')
+
+            if i in masked_tokens:
+                # preprocessing
+                r = np.random.random()
+                if r <= 0.8:
+                    inputs.append('[MASK]')
+                    labels.append(i)
+                elif r <= 0.9:
+                    inputs.append(np.random.choice(list(text)))
+                    labels.append(i)
+                else:
+                    inputs.append(i)
+                    labels.append(i)
+                masked_tokens.remove(i)
+
+            else:
+                inputs.append(i)
+                labels.append('[PAD]')
+
+        return [inputs, labels]
+
+    def get_masked_data(self, data):
+        """
+        data = [q1,q2,q3,...]
+
+        return: [[input_1, label_1],[input_2, label_2],...]
+        """
+        res = [self.get_masked_text(i) for i in data]
+
+        self.logger('Inputs & labels:')
+        for i in range(5):
+            self.logger(res[i])
+
+        return res
+
+data_processor = DataProcessor()
