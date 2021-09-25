1b49b46bcde86a7cb01b77a9a96d7c91db78d4cc
Leroll <senjaer1919@gmail.com>
2021-09-25 21:30:50 +0800

    大的版本更新,整合工具类,删去不必要的过程类.

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
 from time import time
-from loguru import logger
-
 
 def time_cost(func):
     def wraps(*args, **kargs):
@@ -9,25 +7,5 @@ def time_cost(func):
         print(f'[{func.__name__}] cost time: {time() - t0:0.3f}s')
         return result
     return wraps
-
-
-class LogGetter:
-    def __init__(self):
-        self.logger = logger
-        self.logger.add('log.log',
-                        enqueue=True,
-                        colorize=True, format="<green>{time}</green> <level>{message}</level>",
-                        level="INFO"
-                        )
-
-    def get_logger(self):
-        return self.logger.info
-
-
-log = LogGetter().get_logger()
-
-if __name__ == '__main__':
-    log('log test')
-
 
 
