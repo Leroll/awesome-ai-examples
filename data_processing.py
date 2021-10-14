38bceb7e6dd857c3bb133b875bfd0d73719af83e
Leroll <senjaer1919@gmail.com>
2021-10-14 10:39:42 +0800

    更改mask的逻辑。之前的做法是先对token，mask，再batch，再tokenizer得到输入，这样就会tokenize 2次，从而导致'##00'这种 word-piece 被再次切分成为'#'，'#'，'00'，从而导致inputs和label的长度不一致，出现问题。现在是更改成了先全部tokenizer成为 token_id之后，再去做mask。

--- data_processing.py
+++ data_processing.py
@@ -257,52 +257,71 @@ class Masker(object):
         self.vocab = tokenizer.vocab
         self.vocab_inverse = reverse_dict(self.vocab)
 
-    def get_masked_text(self, text):
-        """生成masked后的输入inputs和对应的输出标签 labels
-        为了利用tokenizer, labels也生成文字序列, 不预测的部分用[PAD]代替
+        # 一些关键token
+        self.mask_id = self.vocab['[MASK]']
+        self.cls_id = self.vocab['[CLS]']
+        self.sep_id = self.vocab['[SEP]']
+        self.pad_id = self.vocab['[PAD]']
+
+    def get_masked_text(self, input_ids: list, exclude_ids: list):
+        """将单样本的input_ids 转换为 masked_input_ids,同时生成对应的 masked_input_labels
+        未mask的label设置为0。注意是元素是token_id，而非token
+
+        args:
+            input_ids: 经过抱抱脸tokenizer之后的input_ids, 包含[CLS][SEP][PAD]等字符
+            exclude_ids: 不需要mask的id，比如101:[CLS], 102:[SEP], 0:[PAD]
+        return:
+            masked_input_ids: 除了[CLS],[PAD],[SEP]等词以外，进行mask 替换
+            masked_labels: 与输入等长,对应的label，只有mask对应的字符有值，其余为0，包括[CLS],[SEP]
+            mask_flag: 用于计算loss时，筛选出需要计算loss的项。类似tokenizer 的attention_mask
+
         """
-        if self.vocab is None:
-            raise Exception('vocab is none, you need to set it before running this func')
+        masked_input_ids = []
+        masked_labels = []
 
-        inputs = []
-        labels = []
-
-        text = self.tokenizer.tokenize(text)  # string 分离个tokens
         # 获取 masked tokens
-        masked_num = max(1, round(len(text) * 0.15))
-        masked_tokens = random.sample(text, masked_num)
+        masked_index_pool = [idx for idx, i in enumerate(input_ids) if i not in exclude_ids]  # 候选池的索引
+        masked_num = max(1, round(len(masked_index_pool) * 0.15))
+        masked_index = random.sample(masked_index_pool, masked_num)
 
-        for i in text:
-            if i in masked_tokens:
+        for idx, i in enumerate(input_ids):
+            if idx in masked_index:
                 r = np.random.random()
                 if r <= 0.8:
-                    inputs.append('[MASK]')
-                    labels.append(i)
+                    masked_input_ids.append(self.mask_id)
                 elif r <= 0.9:
-                    inputs.append(np.random.choice(list(text)))
-                    labels.append(i)
+                    replace_id = input_ids[np.random.choice(list(masked_index_pool))]
+                    masked_input_ids.append(replace_id)
                 else:
-                    inputs.append(i)
-                    labels.append(i)
-                masked_tokens.remove(i)
+                    masked_input_ids.append(i)
+                masked_labels.append(i)
             else:
-                inputs.append(i)
-                labels.append('[PAD]')
+                masked_input_ids.append(i)
+                masked_labels.append(self.pad_id)
 
-        # 转回字符串, 字符之间会多出空格，但是没关系tokenizer后续会忽略
-        inputs = self.tokenizer.convert_tokens_to_string(inputs)
-        labels = self.tokenizer.convert_tokens_to_string(labels)
-        return [inputs, labels]
+        mask_flag = [int(i > 0) for i in masked_labels]
+        return [masked_input_ids, masked_labels, mask_flag]
 
-    def get_masked_data(self, data: list):
-        """
+    def get_masked_data(self, inputs: dict, exclude_ids: list):
+        """对batch进行mask
+
         args:
-            data = [q1,q2,q3,...]
+            inputs: 抱抱脸tokenizer 的返回值
+
         return:
-            [[input_1, label_1],[input_2, label_2],...]
+            x: dict, 和tokenizer返回的一致
+                {'input_ids': , 'token_type_ids': , 'attention_mask':}
+            y: torch.tensor, masked_label_token_id, 非mask 的token id 为 0
+            y_mask: torch.tensor, bool值的list，指示哪些字符用于计算loss
         """
-        res = [self.get_masked_text(i) for i in data]
-        return res
+        mask_res = [self.get_masked_text(i, exclude_ids) for i in inputs['input_ids']]
+        masked_input_ids, masked_labels, mask_flags = tuple(zip(*mask_res))
+
+        inputs['input_ids'] = torch.tensor(masked_input_ids).to(self.device)
+        x = inputs
+        y = torch.tensor(masked_labels).to(self.device)
+        y_mask = torch.tensor(mask_flags).to(self.device)
+        return x, y, y_mask
 
     def get_token_from_single(self, q, is_split_into_words=False):
         tokens = self.tokenizer(text=q,
@@ -325,21 +344,6 @@ class Masker(object):
                                 ).to(self.device)
         return tokens
 
-    def get_y_mask(self, y):
-        """
-        like attention_mask, get y_mask from y_token
-        """
-        y_mask = []
-        for i in y:
-            temp_mask = []
-            for j in i:
-                m = 0 if j in [0, 101, 102] else 1
-                temp_mask.append(m)
-            y_mask.append(temp_mask)
-
-        y_mask = torch.tensor(y_mask).to(self.device)
-        return y_mask
-
     def trans_to_masked_data(self, batch: list):
         """把初步数据集转换为 masked 数据集
 
@@ -353,13 +357,10 @@ class Masker(object):
             y_mask: bool值的list，指示哪些字符用于计算loss
         """
         q = batch
-        q_mask_res = self.get_masked_data(q)
-        q_mask_input, q_mask_label = list(zip(*q_mask_res))
+        inputs = self.get_token_from_single(q, is_split_into_words=False)
+        exclude_ids = [self.cls_id, self.sep_id, self.pad_id]
 
-        x = self.get_token_from_single(q_mask_input, is_split_into_words=False)
-        y = self.get_token_from_single(q_mask_label, is_split_into_words=False)['input_ids']
-        y_mask = self.get_y_mask(y)
-
+        x, y, y_mask = self.get_masked_data(inputs, exclude_ids)
         return x, y, y_mask
 
     def trans_to_pet_masked_data(self, batch: list):
@@ -373,6 +374,10 @@ class Masker(object):
             2. 因为想要根据每个batch的最大长度来打padding，所以不能对全部数据处理完之后，再生成dataloader
             3. 直接根据原始的数据集，生成dataloader，然后配合collate_fn在每次循环的时候处理
             4. 由于mask也是在这里做的，所以就意味着, 每一次mask的结果都不一样，可以直接多次循环
+            5. 之前的做法是先对token，mask，再batch，再tokenizer得到输入，这样就会tokenize 2次
+               从而导致'##00'这种 word-piece 被再次切分成为'#'，'#'，'00'，从而导致inputs和label的长度
+               不一致，出现问题。
+               现在是更改成了先全部tokenizer成为 token_id之后，再去做mask。
 
         test:
             1. test阶段文本就不需要做新的label了，直接输入tokenize一下
@@ -388,16 +393,12 @@ class Masker(object):
         """
         if len(batch[0]) == 3:  # training
             q1, q2, label = list(zip(*batch))
+
+            inputs = self.get_token_from_pair(q1, q2)
+            exclude_ids = [self.cls_id, self.sep_id, self.pad_id]
+            x, y, y_mask = self.get_masked_data(inputs, exclude_ids)
+
             # TODO, 对于mask的比例，先每句话都mask，后续要看看pet的原论文，里面具体mask的比例
-            q1_mask_res = self.get_masked_data(q1)
-            q1_mask, q1_mask_label = list(zip(*q1_mask_res))
-
-            q2_mask_res = self.get_masked_data(q2)
-            q2_mask, q2_mask_label = list(zip(*q2_mask_res))
-
-            x = self.get_token_from_pair(q1_mask, q2_mask)
-            y = self.get_token_from_pair(q1_mask_label, q2_mask_label)['input_ids']
-
             # 更新 y 的 cls label
             label_map = {
                 1: '是', 0: '否', -1: '[PAD]'
@@ -405,9 +406,7 @@ class Masker(object):
             for idx, i in enumerate(label):
                 cls_id = self.vocab[label_map[i]]
                 y[idx][0] = cls_id
-
-            y_mask = self.get_y_mask(y)
-
+                y_mask[idx][0] = 1
             return x, y, y_mask
         else:
             q1, q2 = list(zip(*batch))
