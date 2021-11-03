f8e625651fb710ca11a1895b6b7bd5917466ec1a
Leroll <senjaer1919@gmail.com>
2021-11-03 09:52:18 +0800

    增加deterministic训练的函数

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 from time import time
+import torch
+import random
+import numpy as np
 
 
 def time_cost(func):
@@ -40,6 +43,29 @@ class ModelConfig(dict):
 
     def __setattr__(self, key, value):
         self[key] = value
+
+
+def set_deterministic(seed=42):
+    # seed
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+    # https://pytorch.org/docs/stable/notes/randomness.html
+    # cudnn那边，使用相同的算法，而不是经过benchmark后在几个中选取最快的。
+    torch.backends.cudnn.benchmark = False
+    # torch内部一些方法使用确定型的算法，具体list见函数文档
+    torch.use_deterministic_algorithms(True)
+    # 当上述cudnn使用同一算法时，有可能算法本身不是确定性的，因此需要下述设定
+    # 但是该设定已经被上面的设定包含了。
+    # torch.backends.cudnn.deterministic = True
+
+    # dataloader在多进程时也会有reproducibility的问题
+    # 这部分暂时不涉及。
+
+
+
+
 
 
 # 暂时把之前的一些废代码放在这
