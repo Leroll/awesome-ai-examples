8ce4939add0cdd29aef9de399f968f5c155e5cb6
Leroll <senjaer1919@gmail.com>
2021-11-05 14:04:54 +0800

    1. 更新可复现开关控制 2. 增加拼音比对函数

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ from time import time
 import torch
 import random
 import numpy as np
+import os
+from pypinyin import lazy_pinyin
+import jieba
 
 
 def time_cost(func):
@@ -46,26 +49,42 @@ class ModelConfig(dict):
 
 
 def set_deterministic(seed=42):
+    """
+    让pytorch训练过程可复现，固定各种随机化的操作。
+    """
     # seed
     torch.manual_seed(seed)
     random.seed(seed)
     np.random.seed(seed)
+    # 禁止hash随机化
+    os.environ['PYTHONHASHSEED'] = str(seed)
 
     # https://pytorch.org/docs/stable/notes/randomness.html
     # cudnn那边，使用相同的算法，而不是经过benchmark后在几个中选取最快的。
     torch.backends.cudnn.benchmark = False
+
     # torch内部一些方法使用确定型的算法，具体list见函数文档
-    torch.use_deterministic_algorithms(True)
+    # torch.use_deterministic_algorithms(True)
+
     # 当上述cudnn使用同一算法时，有可能算法本身不是确定性的，因此需要下述设定
     # 但是该设定已经被上面的设定包含了。
-    # torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.deterministic = True
 
     # dataloader在多进程时也会有reproducibility的问题
     # 这部分暂时不涉及。
 
 
-
-
+def compare_pinyi(s1: str, s2: str):
+    """
+    对比两句话是否拼音完全一致。
+    """
+    s1_pinyin = ""
+    s2_pinyin = ""
+    for w in jieba.cut(s1):
+        s1_pinyin += ''.join(lazy_pinyin(w))
+    for w in jieba.cut(s2):
+        s2_pinyin += ''.join(lazy_pinyin(w))
+    return s1_pinyin == s2_pinyin
 
 
 # 暂时把之前的一些废代码放在这
