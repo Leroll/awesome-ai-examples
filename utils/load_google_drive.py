2c5d9fb27084bb6b9d895724e92f932306742c70
Leroll <senjaer1919@gmail.com>
2023-10-12 20:49:54 +0800

    更新清缓存函数

--- utils/load_google_drive.py
+++ utils/load_google_drive.py
@@ -1,8 +1,14 @@
+
+
+#---------------------------------
+#  colab 加载 google drive
+#--------------------------------- 
 from google.colab import drive
 import os
-
 def load_google_drive(path='/'):
-    """path 为 my google drive 内部路径 
+    """
+    Args:
+        path:  my google drive 内部路径 
     """
 
     drive.mount('/content/drive')
@@ -10,3 +16,15 @@ def load_google_drive(path='/'):
     
     os.chdir(root_path + path)
     print(f"cwd: {os.getcwd()}")
+
+
+#---------------------------------
+#  Function to clean RAM & vRAM
+#--------------------------------- 
+import gc
+import ctypes
+import torch
+def clean_memory():
+    gc.collect()
+    ctypes.CDLL("libc.so.6").malloc_trim(0)
+    torch.cuda.empty_cache()
